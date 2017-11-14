e4fa721ef7bae38c15ef7ab5e64b74d09fa841d2
v.karyagin <tripolis777@gmail.com>
2017-11-14 18:34:54 +0300

    add Realm

---
+++ app/src/main/java/com/android/tripolis/culturefreeintellecttest/Database/Query/JoinQuery.java
@@ -0,0 +1,10 @@
+package com.android.tripolis.culturefreeintellecttest.Database.Query;
+
+/**
+ * Created by v.karyagin on 14.11.2017.
+ */
+
+public class JoinQuery {
+
+    public JoinQuery(String joinQuery, int columnsCount) {}
+}
